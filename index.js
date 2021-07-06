41baef9883752ef9221f85b959ae6c7eba8efdea
Ridam Garg <ridamgarg8979@gmail.com>
2021-07-07 02:55:54 +0530

    First Commit

--- index.js
+++ index.js
@@ -96,6 +96,12 @@ app.post(
     });
   }
 );
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen("5000", () => {
   console.log("Connected");
 });
